c4ddf28af63e6418d4c32892a05a454335dfd792
Chris Breaux and Nayomi Morita <nayomi@>
2019-05-01 13:25:20 -0700

    refactor into separate components and webpack

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Switch, Route, Link } from 'react-router-dom';
+import Game from './Game';
+
+function App() {
+  return(
+    <div>
+      <Switch>
+        <Route path='/' component={Game} />
+      </Switch>
+    </div>
+  )
+}
+
+export default App
